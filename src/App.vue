6a3b35b7303a3c089a5bb524d98963ccdc21df79
William Horning <w.horning27@gmail.com>
2022-02-04 11:03:56 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav>
+      <router-link to="/">openly (BETA)</router-link>
+    </nav>
+    <main>
+      <router-view />
+    </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,scss}'],
+  theme: {
+    fontFamily: {
+      openly: ['Openly'],
+    },
+    backgroundImage: {
+      openly: 'linear-gradient(120deg, #008cc4, #008080)',
+    },
+  },
+  plugins: [],
+};
