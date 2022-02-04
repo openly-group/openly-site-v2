20d4469838c147f488942580a518b889cd02e6c5
William Horning <w.horning27@gmail.com>
2022-02-04 11:23:25 -0500

    fixes and stuff:tm:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,4 @@
     openly is in beta, and the database is being moved to the Meower
     Server, so uploading code is unavalible.
   </p>
-  <br>
-  <router-link to="/changelog">
-    <button><i class="fa-solid fa-plus"></i> What's New?</button>
-  </router-link>
-  <router-link to="/about">
-    <button><i class="fa-solid fa-question"></i> About This Website</button>
-  </router-link>
-  <router-link to="/terms-of-service">
-    <button><i class="fa-solid fa-exclamation"></i> Terms of Service</button>
-  </router-link>
 </template>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,4 +1,4 @@
 <template>
-<h1>404 Not Found</h1>
-<router-link to="/">Go Home</router-link>
+  <h1>404 not found</h1>
+  <router-link to="/"><button>go home</button></router-link>
 </template>
